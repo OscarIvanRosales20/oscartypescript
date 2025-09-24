08f36353da51508215d19145bb230eab24b9108a
KARYUX <l18550734@chihuahua2.tecnm.mx>
2025-09-24 02:28:56 -0600

    Proyecto Base de React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Proyecto Base React + TypeScript
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
